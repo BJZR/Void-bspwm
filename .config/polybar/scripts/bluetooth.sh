b8167d1f4cd589bebd5384dfbd53ff291a37d480
BJZR <132262356+BJZR@users.noreply.github.com>
2025-06-10 20:11:48 -0500

    Add files via upload

--- .config/polybar/scripts/bluetooth.sh
+++ .config/polybar/scripts/bluetooth.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+if bluetoothctl show | grep -q "Powered: yes"; then
+    if bluetoothctl info | grep -q "Connected: yes"; then
+        echo " Conectado"
+    else
+        echo " Encendido"
+    fi
+else
+    echo " Apagado"
+fi
