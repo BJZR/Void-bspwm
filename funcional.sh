7b67348a0955df8e2262909d6aaa923befb795bc
BJZR <132262356+BJZR@users.noreply.github.com>
2025-06-29 21:47:55 -0500

    Create funcional.sh

---
+++ funcional.sh
@@ -0,0 +1,6 @@
+cd ~
+chmod +x ~/.config/bspwm/bspwmrc
+chmod +x ~/.config/sxhkd/sxhkdrc
+chmod +x ~/.config/polybar/launch.sh
+chmod +x ~/.config/slock/slock.sh
+chmod +x ~/.config/polybar/scripts/bluetooth.sh
